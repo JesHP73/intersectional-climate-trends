e5fd4492f188364bfc18cd9233303eddb6bd85c2
Jes <143019142+JesHP73@users.noreply.github.com>
2023-11-28 00:50:14 +0100

    Add files via upload

---
+++ pages/app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+
+st.markdown("# Welcome to the Intersectional Climate Trends App")
+st.sidebar.markdown("# Navigation")
+
